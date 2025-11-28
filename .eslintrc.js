d2ffa724817e239d04cfc431da7c5d3a71aa812d
illiaboiko <illiaboiko.095@gmail.com>
2025-11-28 16:37:08 +0200

    fixed code to pass tests: fixed Router import statements typos and rendering next lint in the PokemonPage.jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
   env: {
+    browser: true,
+    node: true,
     es6: true,
     'jest/globals': true,
   },
-
   extends: ['eslint:recommended', 'plugin:react/recommended'],
-
   parserOptions: {
-    ecmaFeatures: { jsx: true },
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-
   plugins: ['react', 'jest'],
-
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
@@ -27,32 +27,4 @@ module.exports = {
     'react/prop-types': 0,
   },
 
-  // ---------------------
-  // OVERRIDES BY CONTEXT
-  // ---------------------
-  overrides: [
-    // FRONTEND (browser) — all frontend JS/JSX in src/
-    {
-      files: ['src/**/*.{js,jsx}'],
-      env: { browser: true },
-    },
-
-    // BACKEND (Node) — your Express server
-    {
-      files: ['app.js'],
-      env: { node: true },
-    },
-
-    // CONFIG FILES — always Node
-    {
-      files: ['webpack.config.js', 'eslintrc.js'],
-      env: { node: true },
-    },
-
-    // TESTS — Jest runs in Node
-    {
-      files: ['test/**/*.js'],
-      env: { node: true },
-    },
-  ],
 }
